c8b3833ce8f1a4d09bee05773e4356e5f2015c44
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-11-14 13:22:47 -0300

    feat: iniciando configuração do projeto

---
+++ src/components/global/sidebar/app-sidebar.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import * as React from "react"
+import { NavMain } from "@/components/global/sidebar/nav-main"
+import { NavUser } from "@/components/global/sidebar/nav-user"
+import { TeamSwitcher } from "@/components/global/sidebar/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { data } from "@/config/sidebar-config"
+
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
